5f1ab60c95225dfc0d7b49c0d614ab6a5c673d0f
k.d.v.kumar123@gmail.com <k.d.v.kumar123@gmail.com>
2020-12-14 20:06:44 +0530

    commit

--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const marioSchema = new mongoose.Schema({
     name: String,
     weight: Number,
-  });
-  
-  const marioModel = mongoose.model("mariochar", marioSchema);
+});
+
+const marioModel = mongoose.model("mariochar", marioSchema);
 
 module.exports = marioModel;
